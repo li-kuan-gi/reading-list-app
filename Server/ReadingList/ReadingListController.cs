16477261fc8e399f71cc29ad2e533658c4ba8640
li-kuan-gi <ie31415926@gmail.com>
2025-05-06 07:22:02 +0000

    basic input validation in AddBookReadingList API

--- Server/ReadingList/ReadingListController.cs
+++ Server/ReadingList/ReadingListController.cs
@@ -9,6 +9,13 @@ public class ReadingListController() : ControllerBase
     [HttpPost("add")]
     public async Task<ActionResult<BookInfo>> AddBookToReadingList(AddingBookDTO dto)
     {
+        string? error = dto.Validate();
+
+        if (error is not null)
+        {
+            return BadRequest(new { error });
+        }
+
         throw new NotImplementedException();
     }
 }
