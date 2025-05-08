10fa8cecea8d931475138a937e61f2d5cdec3207
li-kuan-gi <ie31415926@gmail.com>
2025-05-08 08:41:25 +0000

    Implement book list component and fetch data from API

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,7 +1,17 @@
 <script setup>
+import BookList from '@/components/BookList.vue';
 import AddBookForm from '../components/AddBookForm.vue'
+
+import { ref } from 'vue';
+
+const remountBookList = ref(false);
+
+const didAddBook = () => {
+  remountBookList.value = !remountBookList.value;
+}
 </script>
 
 <template>
-  <AddBookForm />
+  <AddBookForm @did-add-book="didAddBook" />
+  <BookList :key="remountBookList" />
 </template>
